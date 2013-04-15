8956d17745960f259fbdaacd24ba9d8a8229e919
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-04-15 15:18:00 -0400

    Adds a threadcount parameter to the Crawler instance
    *separates grabbing of urls from the Crawler class
    
    Bugs:
    *does not crawl all of the pages (terminates before the last link crawled)
    *does not kill all of the excess threads at the termination of the program (fails threads_die unittest w/ 7 threads):
    	turbocrawl [master *%]\ $ python turbocrawler.py
    	I've opened http://www.google.com
    	I've opened http://www.amazon.com
    	I've opened http://www.nytimes.com
    	I've opened http://www.racialicious.com
    	I've opened http://www.groupon.com
    	F
    	======================================================================
    	FAIL: test_threads_die (__main__.CrawlerTests)
    	----------------------------------------------------------------------
    	Traceback (most recent call last):
    	  File "turbocrawler.py", line 65, in test_threads_die
    	    self.assertEqual(1,threading.active_count())
    	AssertionError: 1 != 2
    
    	----------------------------------------------------------------------
    	Ran 1 test in 1.735s
    
    	FAILED (failures=1)
    
    *with fewer threads than urls (threads =4), all of the urls get crawled, but the program never terminates
    	turbocrawl [master *+%]\ $ python turbocrawler.py
    	I've opened http://www.google.com
    	I've opened http://www.nytimes.com
    	I've opened http://www.amazon.com
    	I've opened http://www.racialicious.com
    	I've opened http://www.groupon.com
    	I've opened http://www.yelp.com
    	^C^Z
    	[1]+  Stopped                 python turbocrawler.py
    	turbocrawl [master *+%]\ $ kill %1
    	[1]+  Terminated: 15          python turbocrawler.py

--- turbocrawler.py
+++ turbocrawler.py
@@ -1,38 +1,75 @@
 import urllib2
 import unittest
+import threading
+from Queue import Queue
 
+def grab(url_queue):
+	# print "Started grabbing. There are {} threads alive".format(threading.active_count())
+	while not url_queue.empty():
+		# print "Current Queue Size: {}".format(url_queue.qsize())
+		try:
+			url = url_queue.get()
+			open_url = urllib2.urlopen(url)
+		except urllib2.HTTPError as e:
+			print "Could not open {url}: {} {}".format(e.errono,e.strerror)
+		else:
+			print "I've opened {}".format(url)
+			# print "I'm on {} and there are {} total threads".format(threading.current_thread(), threading.active_count())
+
+			# print "There are {} active threads".format(threading.active_count())
+			# url_queue.task_done()
+	url_queue.task_done()
+
+
+	
 class Crawler:
 	"""Crawler will take a given page as input, 
 	grab its contents and spit them out to some outside process"""
 
-	def __init__(self):
+	def __init__(self,url_list,threads=2):
+		self.urls = Queue(len(url_list))
+		for url in url_list:
+			self.urls.put(url)
+		self.num_threads = threads
+
+	def start_crawl(self):
+		for _ in xrange(self.num_threads):
+			t = threading.Thread(target=grab,args=(self.urls,))
+			t.daemon = True
+			t.start()
+			# print "Started {}".format(t.name)
+		self.urls.join()
+
+	def ship_data(self,data):
 		pass
 
-	def grab(self,url):
-		print url
-		try:
-			open_url = urllib2.urlopen(url)
-		except urllib2.HTTPError as e:
-			print "Could not open {url}: {} {}".format(e.errono,e.strerror)
-		return open_url.read()
 
 
 
 class CrawlerTests(unittest.TestCase):
 	def setUp(self):
-		self.c = Crawler()
-		self.urls = ['http://www.google.com','http://www.amazon.com','http://www.nytimes.com','http://www.racialicious.com','http://www.groupon.com','http://www.yelp.com']
+		urls = ['http://www.google.com','http://www.amazon.com','http://www.nytimes.com','http://www.racialicious.com','http://www.groupon.com','http://www.yelp.com']
+		self.c = Crawler(urls,7)
 
-	def test_grab(self):
-		"""Tests that data returns for all urls being fed in"""
-		counter = 0
-		for url in self.urls:
-			if self.c.grab(url):
-				counter +=1
-		self.assertEqual(len(self.urls),counter)
+	# def test_grab(self):
+	# 	"""Tests that data returns for all urls being fed in"""
+	# 	counter = 0
+	# 	self.c.start_crawl()
+	# 	for url in self.urls:
+	# 		if self.c.grab(url):
+	# 			counter +=1
+	# 	self.assertEqual(len(self.urls),counter)
+
+	def test_threads_die(self):
+		self.c.start_crawl()
+		self.assertEqual(1,threading.active_count())
+
 
 if __name__ == '__main__':
 	unittest.main()
+	
 
 
+
+	
 
