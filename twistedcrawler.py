4b60d703ab42b6fde7a23b5b901df994cbdba441
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-04-24 16:34:30 -0400

    Adds DeferredList to twisted crawler's fetch_urls function

--- twistedcrawler.py
+++ twistedcrawler.py
@@ -1,21 +1,30 @@
 from twisted.web.client import getPage
-from twisted.internet.defer import Deferred
+from twisted.internet.defer import DeferredList
 # from twisted.python.util import println
 
 
 def process_page(output):
 	print "Page Output:{}".format(output[:100])
+	return output
 
 def process_error(output):
 	print "Error: {}".format(output)
 
+def all_processed(result):
+	print "Everything has returned\nResults:{}".format(result)
+
 def fetch_urls(reactor,url_list):
+	prep_list = []
 	for url in url_list:
 		print "Fetching {}".format(url)
 		d = getPage(url)
 		d.addCallback(process_page)
 		d.addErrback(process_error)
-	return d
+		prep_list.append(d)
+	d_list = DeferredList(prep_list)
+	d_list.addCallback(all_processed)
+	return d_list
+
 
 
 if __name__ == '__main__':
