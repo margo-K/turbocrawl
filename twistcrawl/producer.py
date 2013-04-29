b282b788f036dccfbc9c4f27b6660472ae8a3705
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-04-29 18:17:14 -0400

    Restructures producer class to accept a destination

--- twistcrawl/producer.py
+++ twistcrawl/producer.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
+from twisted.web.client import getPage
+from twisted.internet.defer import DeferredList
+from twisted.internet import reactor
 
 from tldextract import extract
+import time
 
 class Producer(object):
 	"""The producer is responsible for taking the seed urls, grabbing their data and sending it to the consumer """
-	def __init__(self,seeds):
-		frontier = {}
+	def __init__(self,seeds,destination):
+		self.frontier = {}
 		for url in seeds:
 			domain = getdomain(url)
 			frontier[domain] = [time.time(),[]] # frontier[domain] = [next crawl time, pages to be crawled]
+		self.destination = destination
 
 	def _getdomain(self,url):
 		"""Returns the domain of a given url"""
@@ -21,14 +26,26 @@ class Producer(object):
 			return True
 		return False
 
-	def fetch_urls(url_list):
+	def _process_page(self,output,url):
+		destination.send((url,output))
+
+	def start(self):
+		self.fetch_urls()
+		reactor.run()
+	
+	def stop(self):
+		reactor.stop()
+
+	def _fetch_urls(self):
 		print "Started fetching"
 		prep_list = []
-		for url in url_list:
-			print "Fetching {}".format(url)
-			d = getPage(url)
-			d.addCallback(process_page,url,time.time())
-			prep_list.append(d)
+		for url in self.frontier:
+			if self.frontier[0] < time.time():
+				print "Fetching {}".format(url)
+				d = getPage(url)
+				d.addCallback(_process_page,url)
+				prep_list.append(d)
 		d_list = DeferredList(prep_list,consumeErrors=True)
 		d_list.addCallback(all_processed)
 		return d_list
+
