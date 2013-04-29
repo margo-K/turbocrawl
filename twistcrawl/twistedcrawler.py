09c61c245b53f94b8bbc39f4d30cf1f6a5cc066a
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-04-29 17:38:50 -0400

    Adds API methods

--- twistcrawl/twistedcrawler.py
+++ twistcrawl/twistedcrawler.py
@@ -4,15 +4,19 @@ import twistcrawl.consumer
 import twistcrawl.producer
 
 def Crawler(object):
-	def __init__(self):
-		self.consumer = Consumer()
+	def __init__(self,indexer=Consumer):
+		self.indexer = indexer() # what role do we want this to have?
+
+	@classmethod
+	def custom(cls,index):
+		return cls(indexer=index)
 
 	def start(self,seeds):
 		self.produce = Producer(seeds)
 
 	def stop(self):
 		"""Eventually, stop could mean stopping the reactor or writing the current raw data (consumer) and 
-		frontier (producer) to a file"""
+		frontier (producer) to a file - Note, should make this compatible so that it reads from a file too"""
 		pass 
 
 if __name__ == '__main__':
